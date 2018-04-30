f64c5d8917b5d6b9c08f2a3a20ace04bbb4c32c4
李 明元 <limy@videon.cn>
2018-04-30 13:35:05 +0800

    update

---
+++ src/main/java/com/recruit/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.recruit.repository.search;
+
+import com.recruit.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
